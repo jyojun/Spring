00361641b460006205df475c1db9172774026bb1
jyojun <parkcode98@gmail.com>
2022-01-12 11:37:12 +0900

    회원 관리 예제

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member save); // 회원을 저장하고, 저장된 회원을 반환
+    Optional<Member> findById(Long id); // null 값도 반환될 수 있기 때문에 optional 사용
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+    void clearStore();
+}
---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // id key값을 생성할 때 필요한 변수
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null값을 반환할 수 있기 때문에, Optional.ofNullable()로 감싼다.
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values()); // store의 values이 Member이다. 모든 Member 출력
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
